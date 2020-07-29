61d9a84e794f63a4bf9bf7943a945119a441f01c
Nathalie <nathaliehenrion@hotmail.com>
2020-07-29 13:22:57 +0200

    home page creation

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import request from 'superagent';
 import names from "./names";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import LoginPage from "./pages/loginPage";
+import HomePage from "./pages/homePage";
+
 
 const apiEndpoint = "https://avatars.dicebear.com/v2/avataaars/";
 const apiOptions = "options[mood][]=happy";
@@ -14,56 +16,16 @@ const loginEndpoint = `${backendUrl}/authentication_token`;
 const getAvatar = name => `${apiEndpoint}${name}.svg?${apiOptions}`;
 
 function App() {
-    const [registeredBeneficiaries, setRegisteredBeneficiaries] = React.useState(
-        []
-    );
-    const fetchBeneficiaries = async () => {
-        const loginResponse = await request('POST', loginEndpoint)
-            .send({ email: 'tester@gmail.com', password: 'I@mTheT€ster' });
-        const response = await request('GET', beneficiariesEndpoint)
-            .auth(loginResponse.body.token, { type: 'bearer' });
-        setRegisteredBeneficiaries(response.body['hydra:member']);
-    };
-
-    React.useEffect(() => {
-        fetchBeneficiaries();
-    }, []);
-    const beneficiaryNames = [...Array(12).keys()].map(number => ({
-        name: names[Math.floor(Math.random() * names.length)]
-    }));
-
+    
     return (
         <Fragment>
             <HashRouter>
                 <Switch>
                     <Route path="/login" component={LoginPage} />
+                    <Route path="/" component={HomePage} />
                 </Switch>
             </HashRouter>
-            <div className="App">
-                <header className="App-header">
-                    <h1>Bienvenue dans le gestionnaire de bénéficaires Reconnect</h1>
-                    <hr />
-                    <h3>Personnes stockées en base</h3>
-                    <div className="Beneficiaries-list">
-                        {registeredBeneficiaries.map((beneficiary) => (
-                            <div className="Beneficiary-card" key={beneficiary.id}>
-                                <img src={getAvatar(beneficiary.name)} alt={beneficiary.name} />
-                                <span>{beneficiary.name}</span>
-                            </div>
-                        ))}
-                    </div>
-                    <hr />
-                    <h3>Personnes non stockées</h3>
-                    <div className="Beneficiaries-list">
-                        {beneficiaryNames.map((beneficiary, index) => (
-                            <div className="Beneficiary-card" key={beneficiary.name + index}>
-                                <img src={getAvatar(beneficiary.name)} alt={beneficiary.name} />
-                                <span>{beneficiary.name}</span>
-                            </div>
-                        ))}
-                    </div>
-                </header>
-            </div>
+            
         </Fragment>
     );
 }
