e0886d15204538462da6b21b3c4b29db447f78bf
Nathalie <nathaliehenrion@hotmail.com>
2020-07-29 22:07:55 +0200

    apiresource on user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,8 +5,10 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
 class User implements UserInterface
