1a2042f64e07ee4ee6a046d361c37ee640909ba6
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2022-04-19 14:43:01 +0200

    ⬆️ [Recipes] Upgrade

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,10 +5,9 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
