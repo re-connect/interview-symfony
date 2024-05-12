e04de040045e066fcecd27a35e92e1c43b1f5339
Laure Bellanger <5022584-laurebll@users.noreply.gitlab.com>
2024-05-12 16:17:29 +0200

    feat: added refresh token and invalidate

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
