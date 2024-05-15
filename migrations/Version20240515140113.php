9634022f0f9e2a879f86314c58825c11defb3bfe
Laure Bellanger <5022584-laurebll@users.noreply.gitlab.com>
2024-05-15 17:03:54 +0200

    feat: added search name filter

---
+++ migrations/Version20240515140113.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240515140113 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__beneficiary AS SELECT id, name, creator, created_at FROM beneficiary');
+        $this->addSql('DROP TABLE beneficiary');
+        $this->addSql('CREATE TABLE beneficiary (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, creator VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        )');
+        $this->addSql('INSERT INTO beneficiary (id, name, creator, created_at) SELECT id, name, creator, created_at FROM __temp__beneficiary');
+        $this->addSql('DROP TABLE __temp__beneficiary');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__beneficiary AS SELECT id, name, creator, created_at FROM beneficiary');
+        $this->addSql('DROP TABLE beneficiary');
+        $this->addSql('CREATE TABLE beneficiary (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, creator VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL)');
+        $this->addSql('INSERT INTO beneficiary (id, name, creator, created_at) SELECT id, name, creator, created_at FROM __temp__beneficiary');
+        $this->addSql('DROP TABLE __temp__beneficiary');
+    }
+}
--- src/Entity/Beneficiary.php
+++ src/Entity/Beneficiary.php
@@ -3,11 +3,16 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use App\Repository\BeneficiaryRepository;
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ApiResource]
 #[ORM\Entity(repositoryClass: BeneficiaryRepository::class)]
+
+#[ApiFilter(SearchFilter::class, properties: ["name" => "ipartial"])] # case insensitive
 class Beneficiary
 {
     #[ORM\Id()]
@@ -17,6 +22,12 @@ class Beneficiary
 
     #[ORM\Column(type: "string", length: 255)]
     private $name;
+
+    #[ORM\Column(length: 255)]
+    private ?string $creator = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
 
     public function getId(): ?int
     {
@@ -31,6 +42,30 @@ class Beneficiary
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreator(): ?string
+    {
+        return $this->creator;
+    }
+
+    public function setCreator(string $creator): static
+    {
+        $this->creator = $creator;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt ?? new DateTime();
 
         return $this;
     }
