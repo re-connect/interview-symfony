9123a6e6b6afabb45e592f7c86b60afde92c8a70
Théophane Durand <git@theophanedurand.eu>
2024-06-10 15:39:14 +0200

    Rollback to working state

--- src/Controller/BeneficiaryController.php
+++ src/Controller/BeneficiaryController.php
@@ -2,36 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Beneficiary;
 use App\Repository\BeneficiaryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\TokenNotFoundException;
-use Symfony\Component\Security\Core\Exception\BadCredentialsException;
-use App\Security\TokenAuthenticator;
 
 class BeneficiaryController extends AbstractController
 {
     /**
      * @Route("/beneficiaries", name="beneficiaries", methods={"GET"})
      */
-    public function index(BeneficiaryRepository $beneficiaryRepository, TokenStorageInterface $tokenStorage, TokenAuthenticator $tokenAuthenticator): JsonResponse
+    public function index(BeneficiaryRepository $beneficiaryRepository): JsonResponse
     {
-        $token = $tokenStorage->getToken();
-
-        if (!$token instanceof TokenInterface) {
-            throw new TokenNotFoundException('No token found');
-        }
-
-        try {
-            $tokenAuthenticator->authenticateToken($token, $tokenStorage);
-        } catch (AuthenticationException $e) {
-            throw new BadCredentialsException('Invalid token');
-        }
-
         $beneficiaries = $beneficiaryRepository->findAll();
 
         return $this->json($beneficiaries, 200, [], ['groups' => 'beneficiary:read']);
